89eb8a227dd8083f9fa90b463d23a4c6e00e2a77
saurabhB0970 <bhosalesaurabh645@gmail.com>
2025-02-21 15:12:13 +0530

    landindpage.jsx

---
+++ landindpage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function LandingPage() {
+  return (
+    <div>
+      <h1>Welcome to the Landing Page</h1>
+      <p>This is the main landing page content.</p>
+    </div>
+  );
+}
+
+export default LandingPage;
