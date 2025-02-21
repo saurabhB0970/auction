e7b3e2f4e5e4242ac8c7d5c343cb64b8591e4c78
saurabhB0970 <bhosalesaurabh645@gmail.com>
2025-02-21 15:12:51 +0530

    signin.jsx

---
+++ signin.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function SignIn() {
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <p>This is the sign-in page.</p>
+      {}
+    </div>
+  );
+}
+
+export default SignIn;
